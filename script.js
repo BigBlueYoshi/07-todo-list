26e2f82babd8a97a90f212372b8188726950999f
BigBlueYoshi <144458971+BigBlueYoshi@users.noreply.github.com>
2024-03-22 09:30:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const todoList = ['make breakfast', 'wash dishes'];
+let todoListHTML = '';
+for(let i = 0; i < todoList.length; i++){
+    const todo = todoList[i];
+    const html = '<p> ${todo} </p>'
+    todoListHTML += html;
+}
+function addTodo(){
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+    todoList.push(name);
+    console.log(todoList);
+    inputElement.value = "";
+}
